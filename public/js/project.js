7917af668df382971b0a181684b7019d9ea0ef78
Swaritdixit <swaritdixit351@gmail.com>
2025-05-26 02:17:33 +0530

    projectaa

--- public/js/project.js
+++ public/js/project.js
@@ -1,18 +1,3 @@
-const projectCards = document.querySelectorAll('.project-card');
-
-projectCards.forEach(card => {
-  const images = card.getAttribute('data-images').split(',');
-  const imageDiv = card.querySelector('.project-image');
-  let index = 0;
-
-  const changeImage = () => {
-    imageDiv.style.backgroundImage = `url(${images[index]})`;
-    index = (index + 1) % images.length;
-  };
-
-  changeImage();
-  setInterval(changeImage, 3000);
-});
 function toggleSidebar() {
   document.body.classList.toggle('sidebar-hidden');
 }
